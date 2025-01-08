c81bfa81f475597b927079b3eec5673aa3f2c3db
Alan Rocha <alanrochaarg2001@gmail.com>
2025-01-08 14:28:48 -0300

    chore: `dotenv`

--- src/serve.js
+++ src/serve.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -12,33 +15,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Api NettCorpSolutions 🚀" });
 });
 
-console.log(
-  "Tentando conectar ao MongoDB com URI:",
-  process.env.MONGODB_URI.replace(/:[^:/@]+@/, ":****@")
-);
-
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Conectado ao MongoDB com sucesso!");
-    console.log("Nome do banco de dados:", mongoose.connection.name);
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.error("Erro ao conectar ao MongoDB:");
-    console.error("Detalhes do erro:", err);
-    process.exit(1);
-  });
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
-app.use("/api/auth", require("./auth/auth"));
-app.use("/api/users", require("./user/user.routes"));
+const authRoutes = require("./auth/auth");
+app.use("/api/auth", authRoutes);
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
