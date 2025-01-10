56056fb358e40b043c25387a9160b1389a71f582
Alan Rocha <alanrochaarg2001@gmail.com>
2025-01-10 10:21:23 -0300

    chore: alterando pastas

--- src/components/config/emailConfig.js
+++ src/components/config/emailConfig.js
@@ -3,17 +3,16 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true para porta 465, false para outras portas
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_APP_PASSWORD, // Senha de App do Google
+    pass: process.env.EMAIL_APP_PASSWORD,
   },
   tls: {
     rejectUnauthorized: false,
   },
 });
 
-// Verificar conexão
 transporter.verify(function (error, success) {
   if (error) {
     console.log("Erro na configuração do email:", error);
